589fb044fb5e008643c639517f0824fb8a9162df
Muhammed Fazil pm <fazilfezz@gmail.com>
2023-10-19 13:44:03 +0530

    last

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -84,7 +84,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/muhammedfazilpm/muhammedfazilportfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
